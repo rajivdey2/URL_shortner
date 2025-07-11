ca0b0dbeb2a5d14ea1d8589e9722596424a90c70
rajivdey2 <deyrajiv2572@gmail.com>
2025-07-11 21:34:14 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function App() {
   const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
@@ -9,7 +11,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://url-shortner22.onrender.com/api/shorten', {
+      const res = await axios.post(`${API_URL}/api/shorten`, {
         fullUrl: url,
       });
       setShortUrl(res.data.shortUrl);
